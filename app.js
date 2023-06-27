410cdc769db669f029a9c09f889a0adde4f5db28
AndrzejHanaj <andrzej.hanaj@op.pl>
2023-06-27 20:50:53 +0200

    commit

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ let gameResult = 0;
 const clickCard = function () {
     
 activeCard = this;
+
+if(activeCard == activeCards[0]) return;
+
 activeCard.classList.remove("hidden");
 
 if(activeCards.length === 0) {
@@ -23,9 +26,33 @@ if(activeCards.length === 0) {
     return;
 }
 else {
-    cards.forEach(card => {
-        card.removeEventListener("click", clickCard)
-    })
+    cards.forEach(card => card.removeEventListener("click", clickCard))
+    activeCards[1] = activeCard;
+    setTimeout(function(){
+        if(activeCards[0].className === activeCards[1].className){
+            console.log("wygrana")
+            activeCards.forEach(card => card.classList.add("off"))
+            gameResult++;
+
+            cards = cards.filter(card => !card.classList.contains("off"))
+
+            if (gameResult == gamePairs){
+               const endTime = new Date().getTime(); 
+               const gameTime = (endTime - startTime)/1000
+               alert(`Udało się! Twój wynik to: ${gameTime} sekund`)
+               location.reload();
+            }
+        }
+        else{
+            console.log("przegrana")
+            activeCards.forEach(card => card.classList.add("hidden"))
+        }
+        activeCard = "";
+        activeCards.length = 0;
+        cards.forEach(card => card.addEventListener("click", clickCard))
+
+    }, 1000)
+
 }
 
 }; 
